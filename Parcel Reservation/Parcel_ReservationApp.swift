bece9d7d3a9abb819f833efba6819161702d88a1
Weixi779 <1349750521@qq.com>
2020-11-19 17:21:17 +0800

    Initial Commit

---
+++ Parcel Reservation/Parcel_ReservationApp.swift
@@ -0,0 +1,20 @@
+//
+//  Parcel_ReservationApp.swift
+//  Parcel Reservation
+//
+//  Created by 孙世伟 on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct Parcel_ReservationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
